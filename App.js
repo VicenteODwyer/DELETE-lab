e5e6e627ede59ace676cf18dbdeb1874e6f22032
VicenteODwyer <viceodwyer@gmail.com>
2024-08-06 15:19:18 -0300

    carpeta-src

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react' ;
+import {StyleSheet, View} from 'react-native';
+import Main from './src/components/main.jsx';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola como esta el widi?</Text>
-      <StatusBar style="auto" />
-    </View>
+     <Main />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text,View } from "react-native";
+
+const Main = () => {
+    return (
+    <view>
+        <text>Cuaderno Digital</text>
+    </view>
+    )
+}
+
+export default Main
