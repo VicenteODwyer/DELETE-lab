08985f25104ca847367ea2b027b66ec4dfac2900
VicenteODwyer <viceodwyer@gmail.com>
2024-10-16 01:44:29 -0300

    FixNavigator

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, View} from 'react-native';
 import LoginForm from './src/components/Iniciar';
 import Interaction from './src/components/Interacciones';
 import { NavigationContainer } from '@react-navigation/native';
-import NotebookCover from './src/components/Inicio';
+import NotebookCover from './src/components/Inicio ';
 
 
 
@@ -14,7 +14,7 @@ export default function App(){
 return (
   <View>
     <NavigationContainer>
-      <NotebookCover/>
+      <Interaction/>
    </NavigationContainer>
     </View>
   );
--- src/components/Interacciones.js
+++ src/components/Interacciones.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import NotebookCover from './Inicio';
 
-// Import other screen components
-import Horarios from './Horarios';
-// ... import other screens
+import NotebookCover from './NotebookCover';
+import DatosAlumno from './DatosAlumno';
+import HorarioEscolar from './HorarioEscolar';
+import Comunicados from './Comunicados';
+import Notas from './Notas';
+import EntradaRetiro from './EntradaRetiro';
 
 const Stack = createStackNavigator();
+
 
 const Interaction = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Index" component={NotebookCover} />
-        <Stack.Screen name="Horarios" component={Horarios} /> 
+      <Stack.Navigator initialRouteName="NotebookCover">
+        <Stack.Screen name="NotebookCover" component={NotebookCover} options={{ title: 'Cuaderno de Comunicaciones' }} />
+        <Stack.Screen name="DatosAlumno" component={DatosAlumno} options={{ title: 'Datos del Alumno' }} />
+        <Stack.Screen name="HorarioEscolar" component={HorarioEscolar} options={{ title: 'Horario Escolar' }} />
+        <Stack.Screen name="Comunicados" component={Comunicados} options={{ title: 'Comunicados' }} />
+        <Stack.Screen name="Notas" component={Notas} options={{ title: 'Notas' }} />
+        <Stack.Screen name="EntradaRetiro" component={EntradaRetiro} options={{ title: 'Entrada y Retiro' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
